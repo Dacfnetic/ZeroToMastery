c3db765fe974e5ceeb4620943d2537e383cd7c31
dacfnetic <dacf9x@gmail.com>
2022-11-12 22:37:05 -0600

    Today

--- scripts/functionsJS.js
+++ scripts/functionsJS.js
@@ -2,11 +2,14 @@ var css = document.querySelector("h3");
 var color1 = document.getElementById("color1");
 var color2 = document.getElementById("color2");
 var b = document.querySelector("body");
+var degress = document.getElementById("grados")
+var bot = document.getElementById("bot")
 
 color1.addEventListener("input",changeBackground);
 color2.addEventListener("input",changeBackground);
+bot.addEventListener("click",changeBackground);
 
 function changeBackground(event){
-	b.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+	b.style.background = "linear-gradient(" + Number(degress.value) + "deg, " + color1.value + ", " + color2.value + ")";
 	css.textContent = b.style.background + ";";
 }
